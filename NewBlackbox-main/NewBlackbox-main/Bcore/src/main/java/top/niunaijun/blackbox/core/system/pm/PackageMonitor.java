0a0a3098dbd7421ab5b9c7e7a2323a328b0b30f2
v54087912-collab <v54087912@gmail.com>
2026-02-14 16:35:20 +0530

    Delete NewBlackbox-main/NewBlackbox-main directory

--- NewBlackbox-main/NewBlackbox-main/Bcore/src/main/java/top/niunaijun/blackbox/core/system/pm/PackageMonitor.java
+++
@@ -1,8 +0,0 @@
-package top.niunaijun.blackbox.core.system.pm;
-
-
-public interface PackageMonitor {
-    void onPackageUninstalled(String packageName, boolean isRemove, int userId);
-
-    void onPackageInstalled(String packageName, int userId);
-}
